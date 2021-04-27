e476a5274549c900d0c42162c34b3281071d4d18
kathmandu777 <mahhakatomanato@gmail.com>
2021-04-27 21:20:58 +0900

    add: generate scaffold Micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210427115040) do
+ActiveRecord::Schema.define(version: 20210427121617) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
